8ed1a87e33211de97bfc9b5c811d3471e9cc3594
yongheL <114266112+yongheL@users.noreply.github.com>
2023-04-09 13:08:34 -0700

    Update index-with-require.js

--- CD220Labs/http_server/index-with-require.js
+++ CD220Labs/http_server/index-with-require.js
@@ -3,7 +3,18 @@ const today = require('./today');
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end(`Hello, World! The date today is ${today.getDate()}`);
+
+  let greetings = "Hello, World"
+
+  if (today.getDate().getHours() > 2 && today.getDate().getHours() < 12) {
+    greetings ="Good morning stranger";
+  } else if (today.getDate().getHours() >= 12 && today.getDate().getHours() < 18) {
+    greetings ="Good afternoon stranger";
+  } else if (today.getDate().getHours() >= 19 && today.getDate().getHours() < 2) {
+    greetings ="Good evening stranger";
+  }
+
+  res.end(`${greetings}, today is ${today.getDate()}`);
 }
 
 const port = 8080;
